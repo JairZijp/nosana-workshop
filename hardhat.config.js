e586b0193a4768f6929ed53385d0a1b763ff61fe
Jairzijp <jairzijp123@gmail.com>
2023-04-17 17:08:58 +0200

    step 3

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-};
+  networks: {
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY || '0'.repeat(64)]
+    }
+  }
+}
